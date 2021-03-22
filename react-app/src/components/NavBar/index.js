423bb88ce19c2f6154a3da15d15f7a4d15b7aed6
bamgithub <48144766+bamgithub@users.noreply.github.com>
2021-03-22 19:03:25 -0400

    profile button working

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,32 +1,12 @@
 import React from "react";
+import Profile from "./Profile"
 
-import { NavLink } from "react-router-dom";
-import LogoutButton from "../auth/LogoutButton";
 import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar__profile">
-        <div className="navbar__profile-button">
-          <i className="far fa-user"></i>
-        </div>
-      </div>
-      <div className="navbar__profile-dropdown">
-        <NavLink to="/" exact={true} activeClassName="active">
-          Home
-        </NavLink>
-        <NavLink to="/login" exact={true} activeClassName="active">
-          Login
-        </NavLink>
-        <NavLink to="/sign-up" exact={true} activeClassName="active">
-          Sign Up
-        </NavLink>
-        <NavLink to="/users" exact={true} activeClassName="active">
-          Users
-        </NavLink>
-        <div><LogoutButton /> </div>
-      </div>
+      <Profile />
     </nav>
   );
 };
