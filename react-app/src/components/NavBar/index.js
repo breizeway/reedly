d62a68072b8980452282172bf8a70f535a495a04
bamgithub <48144766+bamgithub@users.noreply.github.com>
2021-03-22 18:35:39 -0400

    icon coloring and css joy

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,38 +1,34 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from '../auth/LogoutButton';
-import "./NavBar.css"
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+import LogoutButton from "../auth/LogoutButton";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+      <div className="navbar__profile">
+        <div className="navbar__profile-button">
+          <i className="far fa-user"></i>
+        </div>
+      </div>
+      <div className="navbar__profile-dropdown">
+        <NavLink to="/" exact={true} activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink to="/login" exact={true} activeClassName="active">
+          Login
+        </NavLink>
+        <NavLink to="/sign-up" exact={true} activeClassName="active">
+          Sign Up
+        </NavLink>
+        <NavLink to="/users" exact={true} activeClassName="active">
+          Users
+        </NavLink>
+        <div><LogoutButton /> </div>
+      </div>
     </nav>
   );
-}
+};
 
 export default NavBar;
