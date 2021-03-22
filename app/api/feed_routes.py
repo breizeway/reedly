ba0312ccfa354941ab0ac334f0be9141c1bb01cf
Sam Hearst <samhearst1013@gmail.com>
2021-03-21 20:55:24 -0400

    stuff

--- app/api/feed_routes.py
+++ app/api/feed_routes.py
@@ -4,3 +4,10 @@ from app.models import Feed
 
 
 feed_routes = Blueprint('feeds', __name__)
+
+
+@feed_routes.route('/')
+@login_required
+def feeds():
+    feeds = Feed.query.all()
+    return {"feeds": [feed.to_dict() for feed in feeds]}
--- app/models/feed.py
+++ app/models/feed.py
@@ -1,5 +1,6 @@
 from .db import db
 from sqlalchemy.schema import ForeignKey
+from sqlalchemy.orm import relationship
 
 class Feed(db.Model):
   __tablename__ = 'feeds'
@@ -7,3 +8,5 @@ class Feed(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   user_id = db.Column(db.Integer, ForeignKey("users.id"), nullable = False)
   feed_name = db.Column(db.String(50), nullable = False)
+
+  user = relationship("User", back_populates="feeds")
