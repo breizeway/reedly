6745c165997908fa7f1c4c3d2c017ccb7393b6de
jamie <jzrust@gmail.com>
2021-03-23 13:38:11 -0400

    added feed sources 4 and 5

--- app/seeds/sources_feeds.py
+++ app/seeds/sources_feeds.py
@@ -2,16 +2,28 @@ from app.models import db, Source, Feed
 
 def seed_sources_w_feeds():
     feed4 = Feed(user_id=2, feed_name='Business')
+    feed5 = Feed(user_id=1, feed_name='food and Tech')
     source1 = Source(source_url='http://feeds.feedburner.com/Techcrunch',
                     alt_name='TechCrunch')
     source2 = Source(source_url='http://fortune.com/finance/feed/',
                     alt_name='Finance')
+    source3 = Source(source_url='http://rss.cnn.com/rss/cnn_tech.rss',
+                    alt_name='CNN-Tech')
+    source4 = Source(source_url='https://www.dailypress.com/arcio/rss/category/food-drink/?query=display_date:[now-7d+TO+now]+AND+revision.published:true&sort=display_date:desc',
+                    alt_name='Food and Drink')
+
     feed4.sources.append(source1)
     feed4.sources.append(source2)
+    feed5.sources.append(source3)
+    feed5.sources.append(source4)
 
     db.session.add(feed4)
+    db.session.add(feed5)
+
     db.session.add(source1)
     db.session.add(source2)
+    db.session.add(source3)
+    db.session.add(source4)
     db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE the users table.
