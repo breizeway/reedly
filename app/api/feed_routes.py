72afd1788caf6f6bd6abec4c4918f089c60c4e90
jamie <jzrust@gmail.com>
2021-03-23 13:04:22 -0400

    successfully added a feed post to action creator

--- app/api/feed_routes.py
+++ app/api/feed_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_login import current_user, login_required
-from app.models import Feed, Source
+from app.models import db, Feed, Source
 
 
 feed_routes = Blueprint('feeds', __name__)
@@ -20,10 +20,13 @@ def feeds():
 @feed_routes.route('/', methods=["POST"])
 def add_feed():
     data = request.json
-    new_feed = Feed(**data)
+    print("data in post route add-feed form", data)
+    dict_current_user = current_user.to_dict()
+    new_feed = Feed(user_id=dict_current_user["id"], feed_name=data["feedName"])
     db.session.add(new_feed)
     db.session.commit()
     return {
         "id": new_feed.id,
+        "user_id": new_feed.user_id,
         "feed_name": new_feed.feed_name,
     }
