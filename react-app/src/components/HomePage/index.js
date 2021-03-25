f7c6fb87eaf45bc9d10934f913458ba979ac7fb2
jamie <jzrust@gmail.com>
2021-03-25 11:31:50 -0400

    some updates to css

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -12,16 +12,13 @@ function HomePage() {
                 <div className="featured-item featured-item-2">item 2</div>
                 <div className="featured-item featured-item-3">item 3</div>
                 <div className="featured-item featured-item-4">item 4</div>
+
+                <div className="card card-item-1">card-item 1</div>
+                <div className="card card-item-2">card-item 2</div>
+                <div className="card card-item-3">card-item 3</div>
+                <div className="card card-item-4">card-item 4</div>
             </div>
 
-            <div className="homepage__feeds-container">
-            </div>
-            <div className="featured-grid-container">
-                <div className="featured-item featured-item-1">item 1</div>
-                <div className="featured-item featured-item-2">item 2</div>
-                <div className="featured-item featured-item-3">item 3</div>
-                <div className="featured-item featured-item-4">item 4</div>
-            </div>
         </>
     )
 }
