985834f129bc60128efb078f703ba8faa84580b2
jamie <jzrust@gmail.com>
2021-03-25 16:16:53 -0400

    grid complete, needs refactoring into components

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -4,21 +4,88 @@ import './HomePage.css';
 function HomePage() {
     return (
         <>
-            <div>This is the home page.</div>
+            <div>Add new sources.</div>
             <div className="homepage__feeds-container">
             </div>
-            <div className="featured-grid-container">
-                <div className="featured-item featured-item-1">item 1</div>
-                <div className="featured-item featured-item-2">item 2</div>
-                <div className="featured-item featured-item-3">item 3</div>
-                <div className="featured-item featured-item-4">item 4</div>
-
-                <div className="card card-item-1">card-item 1</div>
-                <div className="card card-item-2">card-item 2</div>
-                <div className="card card-item-3">card-item 3</div>
-                <div className="card card-item-4">card-item 4</div>
+            <div className="grid-container">
+                <div className="featured-item">item 1</div>
+                <div className="featured-item">item 2</div>
+                <div className="featured-item">item 3</div>
+                <div className="featured-item">item 4</div>
+                <div className="group-container-heading">Industries</div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <div className="card-header">Financial Services</div>
+                        <div className="card-footer">Featuring Calculated Risk</div>
+                    </div>
+                </div>
             </div>
-
         </>
     )
 }
