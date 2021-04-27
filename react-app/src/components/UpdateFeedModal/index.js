a1ecda0f1a4307e2d26ea4c21fb6a2c99668bf84
Sam Hearst <samhearst1013@gmail.com>
2021-04-27 11:55:04 -0400

    handleSubmit

--- react-app/src/components/UpdateFeedModal/index.js
+++ react-app/src/components/UpdateFeedModal/index.js
@@ -1,15 +1,30 @@
 import React, { useState } from "react"
-import { useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { useParams, useHistory } from "react-router-dom"
 import "./UpdateFeedModal.css"
 
 function UpdateFeedModal() {
+    const dispatch = useDispatch()
+    const history = useHistory();
     const { feedId } = useParams();
     const feed = useSelector(state => state.feeds[Number(feedId)]);
     const [ feedName, setFeedName ] = useState(feed.feed_name)
 
     function updateFeedName(e) {
         setFeedName(e.target.value)
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const payload = {
+            feed_name: feedName,
+            feed_id: feed.id,
+        }
+
+        await dispatch(updateFeed(payload))
+
+        history.push(`/feeds/${feed.id}`)
     }
 
     if (!feed) {
@@ -21,7 +36,7 @@ function UpdateFeedModal() {
         <div className="form-container">
             <div className="form-container__subheader">Manage</div>
             <div className="form-container__header">{feed.feed_name}</div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-container__input-fields">
                     <span className='form-container__label'>Title</span>
                     <input
