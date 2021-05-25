a64ba23a043844874997b5f78ffd97fcaae4a5f5
Sam Hearst <samhearst1013@gmail.com>
2021-05-24 23:59:40 -0400

    adding functionality to sidebar

--- react-app/src/components/SideBar/index.js
+++ react-app/src/components/SideBar/index.js
@@ -8,11 +8,29 @@ import "./SideBar.css"
 import AddFeed from "../AddFeed"
 import Modal from "../Modal"
 import * as modalActions from "../../store/modal"
+import { load } from "../../store/sidebar";
 
 const SideBar = () => {
     const dispatch = useDispatch();
     const feeds = useSelector(state => state.feeds)
     const feedsArr = Object.values(feeds);
+    const selectedNews = useSelector(state => state.sidebar);
+
+    console.log(selectedNews);
+
+    const isSelected = (selectedNews) => {
+        (function () {
+            if (selectedNews.selected === "all") {
+                return `--selected`; 
+            }
+        })();
+    }
+
+    async function selected(feedOrSource) {
+        console.log("working", feedOrSource);
+
+        await dispatch(load(feedOrSource));
+    }
 
 
     useEffect(() => {
@@ -22,9 +40,9 @@ const SideBar = () => {
     }, [dispatch]);
 
     const modal = {
-      thisVal: 'sidebar/addFeed',
-      val: useSelector(state => state.modal.active),
-      set: () => dispatch(modalActions.setActive(modal.thisVal))
+        thisVal: 'sidebar/addFeed',
+        val: useSelector(state => state.modal.active),
+        set: () => dispatch(modalActions.setActive(modal.thisVal))
     }
 
     return (
@@ -44,7 +62,7 @@ const SideBar = () => {
                     <div className='sidebar__header'>Feeds</div>
                 </div>
                 <div className="sidebar__feed-container">
-                    <div className="feed-list-all sidebar__row">
+                    <div onClick={() => selected('all')} className={`feed-list-all sidebar__row ${isSelected}`}>
                         <div className="sidebar__icon-bars sidebar__icon">
                             <i className="fas fa-bars"></i>
                         </div>
